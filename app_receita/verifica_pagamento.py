2d70daeb30c99d15f9f34ed73a04bd65e65f3dd2
eucandre <eucandre@gmail.com>
2017-06-24 13:47:30 -0300

    edição do dashboard

--- app_receita/verifica_pagamento.py
+++ app_receita/verifica_pagamento.py
@@ -5,13 +5,15 @@ from datetime import date
 
 
 def getNomeOdonto(nr_id):
-    '''o parametro identifica o contrato do cliente'''
-    obj_contrato_odonto = Contrato_odonto.objects.get(pk=nr_id)
+    '''o parametro identifica o contrato do cliente.
+    Este metodo vai buscar os dias em que o cliente esta em dias.'''
 
+    obj_contrato_odonto = Contrato_odonto.objects.get(pk=nr_id)
+    obj_recebimento_odonto = RecebimentoPlano.objects.get(pk=nr_id)
     hoje = date.today()
 
     if obj_contrato_odonto.date_payment_per_month == hoje.day:
         return 0
 
     if obj_contrato_odonto.date_payment_per_month > hoje.day:
-        return int(obj_contrato_odonto.date_payment_per_month) - hoje.day
+        return hoje.day - int(obj_contrato_odonto.date_payment_per_month)
